3b0ce772aeca1b15a7904b95383128f6ca9c1876
chari00 <macharitocruzuk@gmail.com>
2024-10-23 00:07:30 +0100

    refactor

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import random
 
 def on_click(event):
-	lucky_number1 = random.randint(1, 59)
-	lucky_number2 = random.randint(1, 59)
-	lucky_number3 = random.randint(1, 59)
-	lucky_number4 = random.randint(1, 59)
-	lucky_number5 = random.randint(1, 59)
-	lucky_number6 = random.randint(1, 59)
-
-	lucky_numbers = f'{lucky_number1}, {lucky_number2}, {lucky_number3}, {lucky_number4}, {lucky_number5}, {lucky_number6}'
-	#add if statement fot check if the number already exist in the lucky number.
-	num = random.randint(1, 59)
-	if str(num) not in lucky_numbers:
-		lucky_numbers += f', {num}'
+	lucky_numbers = []
+	while len(lucky_numbers) < 6:
+		num = random.randint(1, 59)
+		if num not in lucky_numbers:
+			lucky_numbers.append(num)
 
 	button = document.getElementById('lucky-button')
 	button.style.animation = 'none'
