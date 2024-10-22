c37588281dd5495cad10cbbafaf2d99e266ee965
chari00 <macharitocruzuk@gmail.com>
2024-10-22 23:45:07 +0100

    add if statement fot check if the number already exist in the lucky number.

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def on_click(event):
 	lucky_number6 = random.randint(1, 59)
 
 	lucky_numbers = f'{lucky_number1}, {lucky_number2}, {lucky_number3}, {lucky_number4}, {lucky_number5}, {lucky_number6}'
+	#add if statement fot check if the number already exist in the lucky number.
+	num = random.randint(1, 59)
+	if str(num) not in lucky_numbers:
+		lucky_numbers += f', {num}'
 
 	button = document.getElementById('lucky-button')
 	button.style.animation = 'none'
